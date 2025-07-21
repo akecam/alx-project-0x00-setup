518db31f023418b8d8a335032098f75b374520dd
Donald Umeh <donaldumeh54@gmail.com>
2025-07-21 03:53:25 +0100

    button set up

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { ButtonProps } from "@/interfaces";
 
 const Button: React.FC<ButtonProps> = ({ title, styles }) => {
-  return <button className={`rounded-${styles}`}>{title}</button>;
+  return (
+    <button className="sm:rounded-sm md:rounded-md lg:rounded-full">
+      {title}
+    </button>
+  );
 };
 
 export default Button;
