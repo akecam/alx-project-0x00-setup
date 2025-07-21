3da19ee00a8a50f741c435a4849dce7cff1dc6df
Donald Umeh <donaldumeh54@gmail.com>
2025-07-21 03:55:23 +0100

    button set up

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { ButtonProps } from "@/interfaces";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
-    <button className="sm:rounded-sm md:rounded-md lg:rounded-full">
+    <button className="sm:rounded-sm lg:rounded-lg rounded-full">
       {title}
     </button>
   );
