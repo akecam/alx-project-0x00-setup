a120443fad4723a4a0c73253fcc58c255d7db39c
Donald Umeh <donaldumeh54@gmail.com>
2025-07-21 03:38:17 +0100

    using components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,13 @@
+import Card from "@/components/Card";
 import React from "react";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
